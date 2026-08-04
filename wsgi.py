3a8d3c7d6dd1973dd3a5915126ed1b08b003d0c0
DaliaDaCruz <dahliacruz3@gmail.com>
2026-08-04 15:24:05 +0000

    Update root wsgi

--- wsgi.py
+++ wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "coffee_cpr.settings")
+sys.path.append(os.path.join(os.path.dirname(__file__), 'coffee_cpr'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_cpr.settings')
 
 application = get_wsgi_application()
