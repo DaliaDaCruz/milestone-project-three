19aed1ea43f53d135e25a5efe727227a1fce5a8d
DaliaDaCruz <dahliacruz3@gmail.com>
2026-08-04 19:42:18 +0000

    fix typos

--- coffee_cpr/coffee_cpr/settings.py
+++ coffee_cpr/coffee_cpr/settings.py
@@ -10,7 +10,7 @@ import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-sys.path.inser(0, str(BASE_DIR))
+sys.path.insert(0, str(BASE_DIR))
 sys.path.insert(p, str(BASE_DIR.parent))
 
 # Load environment variables if env.py exists (local dev)
