efae0317c4444ab62aeb1c0a8ae63836fc9330d0
DaliaDaCruz <dahliacruz3@gmail.com>
2026-08-04 23:29:31 +0000

    Fix WSGI sys.path and duplicate Procfiles for Heroku

--- coffee_cpr/coffee_cpr/wsgi.py
+++ coffee_cpr/coffee_cpr/wsgi.py
@@ -4,11 +4,10 @@ from pathlib import Path
 
 from django.core.wsgi import get_wsgi_application
 
-# Calculate base path to ensure Gunicorn resolves nested modules
 BASE_DIR = Path(__file__).resolve().parent.parent
 sys.path.append(str(BASE_DIR))
-sys.path.append(str(BASE_DIR.parent))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_cpr.settings')
 
 application = get_wsgi_application()
+Step 3: Force Comm
