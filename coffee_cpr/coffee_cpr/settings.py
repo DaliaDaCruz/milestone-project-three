2fc440e4c0f3970b66822fa11911dc78c68a52f3
DaliaDaCruz <dahliacruz3@gmail.com>
2026-08-05 00:22:59 +0000

    Fix ROOT_URLCONF path for subfolder execution

--- coffee_cpr/coffee_cpr/settings.py
+++ coffee_cpr/coffee_cpr/settings.py
@@ -50,7 +50,8 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "coffee_cpr.urls"
+ROOT_URLCONF = "urls"
+WSGI_APPLICATION = "wsgi.application"
 
 TEMPLATES = [
     {
