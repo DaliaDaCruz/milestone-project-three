010c072a920031b39ea8b5e40dba75cd0e23ac81
DaliaDaCruz <dahliacruz3@gmail.com>
2026-08-05 21:10:54 +0000

    add root url

--- core/urls.py
+++ core/urls.py
@@ -1,22 +1,11 @@
-# Imports MUST be at the very top of the file (Fixes E402)
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ... existing paths above ...
-    path(
-        'cart/remove/<int:machine_id>/',
-        views.remove_from_cart,
-        name='remove_from_cart'
-    ),
+    path('', views.home, name='home'),
+ path('cart/remove/<int:machine_id>/', views.remove_from_cart, name='remove_from_cart'),
     path('checkout/', views.checkout, name='checkout'),
-    path(
-        'order-success/<str:order_number>/',
-        views.order_success,
-        name='order_success'
-    ),
-
-    # AUTH PATHS (Fixes E131 alignment)
+    path('order-success/<str:order_number>/', views.order_success, name='order_success'),
     path('register/', views.register, name='register'),
     path('logout/', views.user_logout, name='user_logout'),
 ]
